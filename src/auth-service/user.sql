87ba0a3655bf7282eac37350395f7c7bcceeee32
tylerjusfly <tylerjusfly1@gmail.com>
2025-04-17 18:19:04 +0100

    remove sql

--- src/auth-service/user.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE users (
-  id SERIAL PRIMARY KEY,
-  email VARCHAR(255) UNIQUE NOT NULL,
-  password VARCHAR(255) NOT NULL
-);
-
--- Insert a test user (password: test123)
-INSERT INTO users (email, password)
-VALUES ('test@example.com', '$2b$10$zfv6Wd0M2txoJQ8FEQAoGecEcN/xg37pglhUu/PUUWa/JmSK5sR5a');
